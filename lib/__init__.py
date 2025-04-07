a8e4329f449f055ec6aec4bd1fb6c0969c506345
Lucas Hoffmann <lucc@posteo.de>
2025-04-07 23:17:17 +0200

    Fix relative import for python >=3.8

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
 
-from dirsize import dirsize
-from gitlab import get_gitlab_instance
-from munin import munin_safe_id
+from .dirsize import dirsize
+from .gitlab import get_gitlab_instance
+from .munin import munin_safe_id
 
 __author__ = 'Matthias Lohr <matthias@lohr.me>'
